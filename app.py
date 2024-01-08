7b6899ec40e993157bdd3cdb3d20399a57dd4ded
Larry Finkelstein <larry.finkelstein@bms.com>
2024-01-07 19:18:48 -0500

    fix: Add --yes to cosign call

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ async def index():
     return """
 <html>
     <head>
-        <title>Welcome to the Location Service API!</title>
+        <title>Welcome to the NEW AND IMPROVED Location Service API!</title>
     </head>
     <body>
         <font size="7">
